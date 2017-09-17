95f8516f690b039a2d77d87f5f67d6e7f559c9c7
leader285431 <leader285431@outlook.com>
2017-09-17 21:01:40 +0700

    Update alpha

--- js/custom.js
+++ js/custom.js
@@ -3,35 +3,38 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Check if there are any nav burgers
-  if ($navbarBurgers.length > 0) {
+  // // Check if there are any nav burgers
+  // if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', () => {
+  //   // Add a click event on each of them
+  //   $navbarBurgers.forEach(function ($el) {
+  //     $el.addEventListener('click', () => {
 
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+  //       // Get the target from the "data-target" attribute
+  //       var target = $el.dataset.target;
+  //       var $target = document.getElementById(target);
 
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+  //       // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+  //       $el.classList.toggle('is-active');
+  //       $target.classList.toggle('is-active');
 
-      });
-    });
-  }
+  //     });
+  //   });
+  // }
 
-  // navbar
+  ////////////
+  // navbar //
+  ////////////
+
   window.addEventListener("scroll", function(){
-    var getNavs = Array.from(document.getElementsByClassName("header__navbar"));
+    var getNavs = Array.from(document.getElementsByClassName("hero_navbar"));
 
-    if (document.body.scrollTop > 80) {
-        getNavs[0].classList.add( 'navbar__js-color' )
-      } else {
-        getNavs[0].classList.remove( 'navbar__js-color' );
+    if (window.scrollY > 590) {
+        getNavs[0].classList.add( 'navbar__js-active' )
+    } else {
+        getNavs[0].classList.remove( 'navbar__js-active' );
       }
   });
 });
